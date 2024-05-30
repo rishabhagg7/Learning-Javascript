adee335f12ffb72d3055b4e17ca9aaa1a63c4d0e
Rishabh Aggarwal <99592097+rishabhagg7@users.noreply.github.com>
2024-05-30 22:20:53 +0000

    operations updated

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -31,3 +31,44 @@ let stringNumber = String(someNumber)
 
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+
+/************* Operations ***************/
+let value = 3
+let negativeValue = -value
+
+console.log(negativeValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3);
+
+let str1 = "hello"
+let str2 = " rishabh"
+let str3 = str1 + str2
+console.log(str3);
+
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+"2");
+console.log("1"+2+2);
+console.log(1+"2"+2);
+console.log(1+2+"2");
+console.log(3+4*5%3);
+
+console.log(true);
+console.log(+true);
+// console.log(true+); //error
+console.log(+"");
+
+let num1,num2,num3
+num1 = num2 = num3 = 2 + 2
+console.table([num1,num2,num3])
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter);
