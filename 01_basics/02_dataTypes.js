5c9c658fbcaf9415bc2095874ce23c2c7ec87730
Rishabh Aggarwal <99592097+rishabhagg7@users.noreply.github.com>
2024-05-30 19:57:09 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use script"; // treat all JS code as newer version
+
+// alert(3+3) // we are using node js not browser
+
+let name = "Rishabh" //string
+let age = 22 //number
+let isLoggedIn = false //boolean
+
+//number => 2 to power 53
+//bigint => large numbers
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => value is not assigned
+//symbol => is component unique, used in react
+
+
+//object
+
+console.log(typeof "Rishabh")
+console.log(typeof 1.1)
+console.log(typeof age)
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined 
+
