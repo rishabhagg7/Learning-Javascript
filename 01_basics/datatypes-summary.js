ffc06bfbf0d0a87fd8ca34ca3ea312a00496502f
Rishabh Aggarwal <99592097+rishabhagg7@users.noreply.github.com>
2024-05-31 06:27:55 +0000

    datatype summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,28 @@
+// Primitive Datatype 
+
+// 7 types:  String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id == anotherId)
+
+const bigInt = 43438943984398534543n
+
+
+// Referenced Datatypes (Non primitive)
+
+// Arrays, Objects, Functions
+
+const heros = ["thor","spiderman","perman"] //arrays
+
+//objects
+let obj = {
+    name:"rishabh",
+    age: 22
+}
+
+//function
+const myfunction = function(){
+    "Hello World"
+}
+
