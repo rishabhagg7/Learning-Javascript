cceb81d89ba90258e465226ac9514d416f4d9a41
Rishabh Aggarwal <99592097+rishabhagg7@users.noreply.github.com>
2024-06-01 07:25:31 +0000

    objects intro

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,38 @@
+// object: singleton, created using constructor
+// object literals
+
+const mySym = Symbol("key1");
+
+const JsUser = {
+    name:"Rishabh",
+    "full name":"Rishabh Aggarwal", //cannot be access from dot operator
+    age:22,
+    location:"Delhi",
+    email:"abc@gmail.com",
+    isLoggedIn:true,
+    lastLoginDays:["Thursday","Monday"],
+    [mySym]:"myKey1" //symbol declared using [] brackets
+}
+
+console.log(JsUser.name);
+console.log(JsUser["name"]);
+console.log(JsUser["full name"]);
+console.log(typeof JsUser[mySym]);
+console.log(JsUser);
+
+JsUser.email = "def@gmail.com"
+console.log(JsUser.email);
+
+JsUser.greeting = function(){
+    console.log("Hello Js user");
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello Js user, ${this.name}`);
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
+
+Object.freeze(JsUser) //values cannot be change
+JsUser.email = "gef@gmail.com" //no error
+console.log(JsUser.email);
