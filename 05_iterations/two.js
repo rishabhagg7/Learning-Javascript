64ee42df1fbeb26d27516de872de26399f86553e
Rishabh Aggarwal <99592097+rishabhagg7@users.noreply.github.com>
2024-06-03 20:28:19 +0000

    while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,18 @@
+let index = 0
+while(index <= 10){
+    console.log(`Value of index is ${index}`);
+    index+=2;
+}
+
+let myArray = ["batman","spiderman","flash"]
+let i = 0
+while(i < myArray.length){
+    console.log(myArray[i]);
+    i = i + 1
+}
+
+let score = 11
+do {
+    console.log(`Score is ${score}`);
+    score += 1
+} while (score <= 10);
